26098788ed50d48c08fae20207d303839b604688
rahulltanvir <tanvirrahul00000@gmail.com>
2026-04-23 00:10:23 +0600

    construct

--- index.php
+++ index.php
@@ -1,21 +1,32 @@
-<?php
- class myClass{
+<?php 
+// class myClassOOP{
+//     public $name;
+//     public $age;
+
+//     public function parson(){
+//         echo "name:". $this->name."\n". "age:". $this->age;
+//     }
+// }
+// $details= new myClassOOP();
+// $details->name="rahul";
+// $details->age=32;
+// $details->parson();============class
+
+class myclass{
     public $name;
     public $age;
 
-    public function parson(){
-        echo "name:". $this->name.",age:". $this->age;
+    public function __construct($name, $age)
+    {
+        $this->name=$name;
+        $this->age=$age;
     }
- }
 
- $details= new myClass();
- $details->name="rahul";
- $details->age=32;
- $details->parson();
-
-
-
-
-
+    public function details(){
+        echo "Name: ". $this->name . "\n" ."Age:". $this->age;
+    }
+}
+$data= new myclass("rahul", 32);
+echo $data->details();
 
 ?>
